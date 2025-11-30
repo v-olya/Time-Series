6ebdf7c045ed5ebf02fd2ad6ec63c08477669f12
Olya Vo <olga.volkova@email.cz>
2025-11-30 11:07:14 +0100

    add a favicon

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -9,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body>
         <header className="app-home">
           <Link href="/">
